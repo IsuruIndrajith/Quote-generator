b01358d4645fcc46b4b94641344bc30d129d86b1
Isuru <isuruindrajith680@gmail.com>
2025-02-26 21:12:49 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('testing');
+// Get Quote From API
+async function getQuote() {
+    const proxyUrl = 'https://api.allorigins.win/get?url=';
+    const apiUrl = encodeURIComponent('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json');
+    
+    try{
+        const response = await fetch(proxyUrl + apiUrl);
+
+        // returning the above response in json Format
+        const data = await response.json();
+
+        // The response comes wrapped, so you'll need to parse the contents
+        const quote = JSON.parse(data.contents);
+
+        console.log(quote);
+
+    }catch(error){
+        console.log('whoops, no quote', error);
+    }
+
+} 
+
+// On Load
+// Calling the above function
+getQuote();
