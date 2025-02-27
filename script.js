471e6363851bafc29c91b3798750061746686e41
Isuru <isuruindrajith680@gmail.com>
2025-02-28 02:48:12 +0530

    CSS loader

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const facebookBtn = document.getElementById('facebook');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 // Get Quote From API
 async function getQuote() {
+    loading();
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
@@ -22,6 +37,8 @@ async function getQuote() {
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText;
+        // Stop loader and show the quote
+        complete();
 
     } catch (error) {
         console.error("Error fetching quote:", error);
@@ -57,3 +74,4 @@ facebookBtn.addEventListener('click', shareToFacebook);
 
 
 getQuote(); // Fetch first quote on page load
+
